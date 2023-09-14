514e6f13b150145eb4e7a1f1df4c270904df86c0
Mel Bourgeois <mason.bourgeois@gmail.com>
2023-09-14 10:37:15 -0500

    WIP use separate thread for wayland

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::thread;
 use std::time::Duration;
 
 use gtk::glib::timeout_add_local;
@@ -7,6 +8,7 @@ mod config;
 mod wayland;
 
 use wayland::KeyboardWriter;
+use wayland_protocols_misc::zwp_input_method_v2::client::zwp_input_method_v2::ZwpInputMethodV2;
 
 const APP_ID: &str = "org.smona.keyboard";
 const SPACING: i32 = 4;
@@ -15,6 +17,8 @@ fn main() -> glib::ExitCode {
     let app = Application::builder().application_id(APP_ID).build();
 
     app.connect_activate(move |app| {
+        let (tx, rx) = glib::MainContext::channel(glib::source::Priority::DEFAULT);
+
         let conn = wayland_client::Connection::connect_to_env()
             .map_err(|_| "Could not connect to wayland socket.")
             .unwrap();
@@ -22,19 +26,40 @@ fn main() -> glib::ExitCode {
         let mut event_queue = conn.new_event_queue();
         let _registry = wl_display.get_registry(&event_queue.handle(), ());
         let mut writer = KeyboardWriter::new(&mut event_queue);
+        let method = writer.input_method.as_ref().unwrap().clone();
         let mut was_active = false;
 
         let inner_app = app.clone();
-        timeout_add_local(Duration::from_millis(300), move || {
-            event_queue.roundtrip(&mut writer).unwrap();
-            let is_active = writer.is_active();
-            if was_active != is_active {
-                if is_active {
-                    build_ui(&inner_app, &writer);
-                } else {
-                    inner_app.active_window().unwrap().set_visible(false);
+        // timeout_add_local(Duration::from_millis(300), move || {
+        //     event_queue.roundtrip(&mut writer).unwrap();
+        //     let is_active = writer.is_active();
+        //     if was_active != is_active {
+        //         if is_active {
+        //             build_ui(&inner_app, &method);
+        //         } else {
+        //             inner_app.active_window().unwrap().set_visible(false);
+        //         }
+        //         was_active = is_active;
+        //     }
+        //     glib::ControlFlow::Continue
+        // });
+        thread::spawn(move || {
+            let mut was_active = false;
+            loop {
+                event_queue.blocking_dispatch(&mut writer).unwrap();
+                let is_active = writer.is_active();
+                if was_active != is_active {
+                    tx.send(is_active).unwrap();
+                    was_active = is_active;
                 }
-                was_active = is_active;
+            }
+        });
+        let inner_app = app.clone();
+        rx.attach(None, move |should_be_open| {
+            if should_be_open {
+                build_ui(&inner_app, &method);
+            } else {
+                inner_app.active_window().unwrap().set_visible(false);
             }
             glib::ControlFlow::Continue
         });
@@ -46,7 +71,7 @@ fn main() -> glib::ExitCode {
     app.run()
 }
 
-fn build_ui(app: &Application, kb: &KeyboardWriter) {
+fn build_ui(app: &Application, kb: &ZwpInputMethodV2) {
     let config = config::get_config()
         .map_err(|e| format!("Failed to load config: {}", e))
         .unwrap();
@@ -71,7 +96,8 @@ fn build_ui(app: &Application, kb: &KeyboardWriter) {
                 .build();
 
             button.connect_clicked(move |_| {
-                kb.send_key(key.to_owned());
+                kb.commit_string(key.to_owned());
+                kb.commit(1)
             });
 
             row_box.append(&button);
