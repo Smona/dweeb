84425c3181b49db6f134e4ef9b2bb6a41c854541
Mel Bourgeois <mason.bourgeois@gmail.com>
2023-09-21 23:32:10 -0500

    Use fs::read_to_string rather than File

--- src/config.rs
+++ src/config.rs
@@ -1,12 +1,10 @@
-use std::io::prelude::*;
-use std::{collections::HashMap, fs::File};
+use std::collections::HashMap;
 
 use serde::Deserialize;
 
 pub fn get_config() -> Result<Config, String> {
-    let mut file = File::open("config.toml").map_err(|_| "Could not find config file.")?;
     let mut contents = String::new();
-    file.read_to_string(&mut contents)
+    std::fs::read_to_string(&mut contents)
         .map_err(|e| format!("Could not read config file: {}", e.to_string()))?;
     Ok(toml::from_str::<Config>(&contents).unwrap())
     // .map_err(|e| format!("Could not parse config file: {}", e.message()))?
